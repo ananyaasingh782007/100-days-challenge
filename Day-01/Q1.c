dc82bd2a348f272630cbe73e8f43be4923c5ef9c
Ananya Singh <ananyaasingh782008@gmail.com>
2026-08-18 07:32:20 +0000

    Day 1 completed

---
+++ Day-01/Q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+//write a program to input two numbers and display their sum.
+
+int a, b, sum;
+
+scanf("%d %d", &a, &b);
+
+sum = a + b;
+
+printf("Sum = %d", sum);
+return 0;
+}
