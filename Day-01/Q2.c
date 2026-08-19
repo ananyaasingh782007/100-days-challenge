aa87915a6cc16b773bf48e165a9306bfbb7f2534
Ananya Singh <ananyaasingh782008@gmail.com>
2026-08-19 19:24:32 +0000

    Fix Day 1 Question 2

--- Day-01/Q2.c
+++ Day-01/Q2.c
@@ -3,7 +3,7 @@ int main()
 {
 int a, b;
 scanf("%d %d", &a, &b);
-printf("Sum=%d, Diff=%d, Product=%d, ", a = b, a - b, a * b);
+printf("Sum=%d, Diff=%d, Product=%d, ", a + b, a - b, a * b);
 if (b != 0)
 printf("Quotient=%d\n", a / b);
 else
