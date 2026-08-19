9ad8e2de2e913186b0294d254e9af5b83dc888d3
Ananya Singh <ananyaasingh782008@gmail.com>
2026-08-19 09:56:16 +0000

    Add Day 1 Question 2

---
+++ Day-01/Q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int a, b;
+scanf("%d %d", &a, &b);
+printf("Sum=%d, Diff=%d, Product=%d, ", a = b, a - b, a * b);
+if (b != 0)
+printf("Quotient=%d\n", a / b);
+else
+printf("Quotient=Undefined\n");
+return 0;
+}
